b85152679248ab4a38f84eff80fd8e3cbe56f3eb
tamcy82 <tamcy82@gmail.com>
2023-03-13 17:38:46 +0800

    Grouped tests

--- haematology.py
+++ haematology.py
@@ -4,19 +4,23 @@
 # Path: haematology.py
 
 # Define coagulation tests
-CoagulationTests = ["pt", "aptt", "inr"]
+CoagulationTests = ["PT", "aPTT", "INR"]
+TypingTests = ["ABO", "RhD", "Phenotyping", "Antibody Screening", "Direct Antiglobulin Test", "Indirect Antiglobulin Test"]
 
 # Match case-insensitive coagulation tests from a given list of string
 # Return a list of matched tests
-def match_coagulation_tests(item, description):
+def match_coagulation_tests(item, description, allTests):
     matched_tests = []
-    for test in description:
-        if test.lower() in CoagulationTests:
-            matched_tests.append(test)
+    # Exact match coagulation tests
+    for index, T in enumerate(CoagulationTests):
+        if T.lower() in item.lower() or T.lower() in description:
+            matched_tests.append(CoagulationTests[index])
     return matched_tests
 
 # Match blood film examination
-def match_blood_film_examination(item, description):
+def match_blood_film_examination(item, description, allTests):
+    if 'examination of blood film' not in item.lower():
+        return []
     matched_tests = ["Blood Film Examination"]
     # Define flag for blast and morphology
     additional_notes = []
@@ -53,10 +57,24 @@ def match_blood_film_examination(item, description):
     return matched_tests
 
 # Mach complete blood picture
-def match_complete_blood_picture(item, description):
-    matched_tests = ["Complete Blood Picture"]
-    if 'reticulocyte' in item.lower() or 'reticulocyte' in description:
-        matched_tests.append("Reticulocyte")
+def match_complete_blood_picture(item, description, allTests):
+    if 'haematology' in item.lower() or 'count' in description:
+        matched_tests = ["Complete Blood Picture"]
+        if 'reticulocyte' in item.lower() or 'reticulocyte' in description:
+            matched_tests.append("Reticulocytes")
+        return matched_tests
+    elif 'reticulocyte' in item.lower() and "Complete Blood Picture" in allTests:
+        return ["Reticulocytes"]
+    else:
+        return []
+
+# Match blood typing
+def match_blood_typing(item, description, allTests):
+    matched_tests = []
+    # Exact match typing tests
+    for index, T in enumerate(TypingTests):
+        if T.lower() in item.lower() or T.lower() in description:
+            matched_tests.append(TypingTests[index])
     return matched_tests
 
 # Match Trephine Biopsy IHC Reporting
--- main.py
+++ main.py
@@ -2,13 +2,18 @@ import csv
 
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
+import haematology
 import os
 import pandas as pd
 
-import haematology
 
 # Import LST excel file into pandas dataframe
 def read_lst(path):
+    df = pd.read_excel(path, header=0)
+    return df
+
+# Load HaematologyTestsDB into pandas
+def load_haematology_tests_db(path):
     df = pd.read_excel(path, header=0)
     return df
 
@@ -39,7 +44,7 @@ def read_folders(path):
             for root2, dirs2, files2 in os.walk(path + '\\' + dir):
                 # Loop through files with index
                 for index, file in enumerate(files2):
-                    # Check if file is an excel file
+                    # Check if file is an Excel file
                     if file.endswith('.xls'):
                         # Check if key exists
                         if dir in folder_dict:
@@ -66,108 +71,141 @@ def is_haematology(text):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+    # Load HaematologyTestsDB
+    HaematologyTestsDBPath = '.\\HaematologyTestsDB.xlsx'
+    HaematologyTestsDB = load_haematology_tests_db(HaematologyTestsDBPath)
     # define local service tracker information
     LSTPath = '\\\\ctc-network.intranet\\dfs\\BIOT\\01 Study Management\\02 Trackers\\Local Services Tracker.xlsm'
     LST = read_lst(LSTPath)
     # define summary services information
-    BPATProjectFolder = '\\\\ctc-network.intranet\\dfs\\BPATCR\\Contract-Archive\\Project'
+    BPATProjectFolder = '\\\\ctc-network.intranet\\dfs\\BPATCR\\Contract-Archive\\Project\\Imago'
     # Define unique test names for haematology
     HaematologyTests = []
+    HaematologyTestsForCtcNo = {}
+    # Create pandas data frame to store service information
+    # ServiceList = pd.DataFrame(columns=['Service', 'Test Interpretation'])
     # Read the first level of BPATProjectFolder
-    for SponsorFolder in os.listdir(BPATProjectFolder):
-        ServiceFolder = BPATProjectFolder + '\\' + SponsorFolder
-        ServiceFolderDict = read_folders(ServiceFolder)
-        # Loop through dictionary
-        for key, value in ServiceFolderDict.items():
-            # Extract first digits before alphabet from key and then stop
-            digits = ''
-            for i in range(len(key)):
-                if key[i].isdigit():
-                    digits += key[i]
-                else:
-                    break
-            # Read summary services excel file
-            ServicePath = value[0] + '\\' + value[1]
-            # Split value by underscore
-            split_string = value[1].split('_')
-            # Get the item from split_string that contains key
-            CtcNo = key
-            for item in split_string:
-                if key in item:
-                    CtcNo = item
-            if CtcNo == key:
-                CtcNo = key + 'HKU1'
-            print("Loading " + CtcNo + "...")
-            # Check file exists
-            if os.path.exists(ServicePath):
-                ServiceDf = read_summary_services(ServicePath)
-                # print(ServiceDf[0:10])
-                if ServiceDf is False:
-                    print("Service interpretation not found in Summary Services")
-                    continue
-                # Check LST
-                # Select row by key in LST
-                site = LST.loc[LST['CTC No.'] == CtcNo]
-                if site.empty:
-                    print('[' + CtcNo + '] Site not found in LST')
-                # Loop through rows in Service
-                # Flag for haematology test
-                HasHaematology = False
-                for index, row in ServiceDf.iterrows():
-                    if is_haematology(row[1]) or is_haematology(row[0]):
-                        # Update flag
-                        HasHaematology = True
-                        # Print item name
-                        print(row[0])
-                        # Clean row[1] using nltk
-                        # Remove stop words
-                        stop_words = set(stopwords.words('english'))
-                        word_tokens = word_tokenize(row[1])
-                        filtered_sentence = [w for w in word_tokens if not w in stop_words]
-                        # Remove punctuation
-                        filtered_sentence = [w for w in filtered_sentence if w.isalnum()]
-                        # Interpret tests
-                        # Coagulation tests
-                        if 'coagulation' in row[0].lower():
-                            # Match coagulation tests
-                            CTest = haematology.match_coagulation_tests(row[0], filtered_sentence)
-                            print(CTest)
-                            # Add all tests to HaematologyTests if not exists
-                            for test in CTest:
-                                # trim test
-                                test2 = test.strip()
-                                if test2 not in HaematologyTests:
-                                    HaematologyTests.append(test2)
-                        elif 'examination of blood film' in row[0].lower():
-                            BloodFilms = haematology.match_blood_film_examination(row[0], filtered_sentence)
-                            print(BloodFilms)
-                            # Add all tests to HaematologyTests if not exists
-                            for test in BloodFilms:
-                                # trim test
-                                test2 = test.strip()
-                                if test2 not in HaematologyTests:
-                                    HaematologyTests.append(test2)
-                        elif 'haematology' in row[0].lower() and 'count' in row[1].lower():
-                            CBC = haematology.match_complete_blood_picture(row[0], filtered_sentence)
-                            print(CBC)
-                            # Add all tests to HaematologyTests if not exists
-                            for test in CBC:
-                                # trim test
-                                test2 = test.strip()
-                                if test2 not in HaematologyTests:
-                                    HaematologyTests.append(test2)
-                        else:
-                            row[0] = row[0].strip()
-                            HaematologyTests.append(row[0])
-                            #print(filtered_sentence)
-                if HasHaematology is False:
-                    print("No haematology test found")
-                elif not site.empty:
-                    # Check whether column 8 is empty
-                    if pd.isnull(site.iloc[0, 8]):
-                        print('Haematology lab not set')
+    #for SponsorFolder in os.listdir(BPATProjectFolder):
+    #    ServiceFolder = BPATProjectFolder + '\\' + SponsorFolder
+    ServiceFolder = BPATProjectFolder
+    ServiceFolderDict = read_folders(ServiceFolder)
+    # Loop through dictionary
+    for key, value in ServiceFolderDict.items():
+        # Extract first digits before alphabet from key and then stop
+        digits = ''
+        for i in range(len(key)):
+            if key[i].isdigit():
+                digits += key[i]
             else:
-                print("File not found: " + ServicePath)
+                break
+        # Read summary services excel file
+        ServicePath = value[0] + '\\' + value[1]
+        # Split value by underscore
+        split_string = value[1].split('_')
+        # Get the item from split_string that contains key
+        CtcNo = key
+        for item in split_string:
+            if key in item:
+                CtcNo = item
+        if CtcNo == key:
+            CtcNo = key + 'HKU1'
+        print("Loading " + CtcNo + "...")
+        # Check file exists
+        if os.path.exists(ServicePath):
+            ServiceDf = read_summary_services(ServicePath)
+            # print(ServiceDf[0:10])
+            if ServiceDf is False:
+                print("Service interpretation not found in Summary Services")
+                continue
+            # Check LST
+            # Select row by key in LST
+            site = LST.loc[LST['CTC No.'] == CtcNo]
+            if site.empty:
+                print('[' + CtcNo + '] Site not found in LST')
+            # Loop through rows in Service
+            # Flag for haematology test
+            HasHaematology = False
+            CurrentHaematologyTest = []
+            for index, row in ServiceDf.iterrows():
+                if is_haematology(row[1]) or is_haematology(row[0]):
+                    # Update flag
+                    HasHaematology = True
+                    # Print item name
+                    print(row[0])
+                    # Append to ServiceList
+                    # ServiceList = ServiceList.append({'Service': row[0], 'Test Interpretation': row[1]}, ignore_index=True)
+                    # Clean row[1] using nltk
+                    # Remove stop words
+                    stop_words = set(stopwords.words('english'))
+                    word_tokens = word_tokenize(row[1])
+                    filtered_sentence = [w for w in word_tokens if not w in stop_words]
+                    # Remove punctuation
+                    filtered_sentence = [w for w in filtered_sentence if w.isalnum()]
+                    # To lower case
+                    filtered_sentence = [w.lower() for w in filtered_sentence]
+                    # Interpret tests
+                    # Match Coagulation tests
+                    CTest = haematology.match_coagulation_tests(row[0], filtered_sentence, CurrentHaematologyTest)
+                    # if CTest is not empty
+                    if len(CTest) > 0:
+                        print(CTest)
+                        # Add all tests to HaematologyTests if not exists
+                        for test in CTest:
+                            # trim test
+                            test2 = test.strip()
+                            CurrentHaematologyTest.append(test2)
+                    # Blood Film Examination
+                    BloodFilms = haematology.match_blood_film_examination(row[0], filtered_sentence, CurrentHaematologyTest)
+                    if len(BloodFilms) > 0:
+                        # Add all tests to HaematologyTests if not exists
+                        print(BloodFilms)
+                        for test in BloodFilms:
+                            # trim test
+                            test2 = test.strip()
+                            CurrentHaematologyTest.append(test2)
+                        continue
+                    # Complete Blood Picture
+                    CBC = haematology.match_complete_blood_picture(row[0], filtered_sentence, CurrentHaematologyTest)
+                    if len(CBC) > 0:
+                        # Add all tests to HaematologyTests if not exists
+                        print(CBC)
+                        for test in CBC:
+                            # trim test
+                            test2 = test.strip()
+                            CurrentHaematologyTest.append(test2)
+                        continue
+                    # Typing
+                    BTyping = haematology.match_blood_typing(row[0], filtered_sentence, CurrentHaematologyTest)
+                    if len(BTyping) > 0:
+                        # Add all tests to HaematologyTests if not exists
+                        print(BTyping)
+                        for test in BTyping:
+                            # trim test
+                            test2 = test.strip()
+                            CurrentHaematologyTest.append(test2)
+                        continue
+                    # None matched
+                    if len(CTest) == 0 and len(BloodFilms) == 0 and len(CBC) == 0 and len(BTyping) == 0:
+                        # Add test to HaematologyTests if not exists
+                        row[0] = row[0].strip()
+                        CurrentHaematologyTest.append(row[0])
+                        print([row[0]])
+                    # Remove duplicates
+                    CurrentHaematologyTest = list(dict.fromkeys(CurrentHaematologyTest))
+            # Set haematology tests for CTC No.
+            HaematologyTestsForCtcNo[CtcNo] = CurrentHaematologyTest
+            # Append to HaematologyTests
+            for test in CurrentHaematologyTest:
+                if test not in HaematologyTests:
+                    HaematologyTests.append(test)
+            if HasHaematology is False:
+                print("No haematology test found")
+            elif not site.empty:
+                # Check whether column 8 is empty
+                if pd.isnull(site.iloc[0, 8]):
+                    print('Haematology lab not set')
+        else:
+            print("File not found: " + ServicePath)
 
     # Print all haematology tests
     # print(HaematologyTests)
@@ -175,3 +213,40 @@ if __name__ == '__main__':
     # with open('HaematologyTests.csv', 'w', newline='') as f:
     #    writer = csv.writer(f)
     #    writer.writerow(HaematologyTests)
+    # print(HaematologyTests)
+
+    # Export ServiceList into csv
+    # ServiceList.to_csv('ServiceList.csv', index=False)
+
+    # Render Test Info for each Ctc No
+    print("Rendering Test Info for each Ctc No...")
+    for Site, T in HaematologyTestsForCtcNo.items():
+        # find test in database
+        print(Site)
+        TestGroup = []
+        for test in T:
+            SearchTest = HaematologyTestsDB.loc[HaematologyTestsDB['test'] == test]
+            # if found
+            if len(SearchTest) > 0:
+                thisGroup = SearchTest.iloc[0, 3]
+                MatchedTest = False
+                # if thisGroup is not empty
+                if thisGroup is not None:
+                    for index, row in enumerate(TestGroup):
+                        if row["TestGroup"] == thisGroup:
+                            MatchedTest = True
+                            row['Tests'].append(SearchTest.iloc[0])
+                            break
+                    if not MatchedTest:
+                        MatchedTest = True
+                        TestGroup.append({'TestGroup': thisGroup, 'Tests': [SearchTest.iloc[0]]})
+                else:
+                    TestGroup.append({'TestGroup': None, 'Tests': [SearchTest.iloc[0]]})
+            else:
+                TestGroup.append({'TestGroup': None, 'Tests': [SearchTest.iloc[0]]})
+        # Print test group
+        print(TestGroup)
+        print("End")
+        print("---------------------------------")
+        # Encode to json
+        # json_data = json.dumps(TestGroup, indent=4, default=str)
